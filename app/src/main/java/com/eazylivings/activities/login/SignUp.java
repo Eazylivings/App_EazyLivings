fcab6f4173c1d440cffb329fa42fa9bbe0acf295
vibek80 <vibek.sharma80@gmail.com>
2016-05-30 00:25:47 +0530

    Sign In

--- app/src/main/java/com/eazylivings/activities/login/SignUp.java
+++ app/src/main/java/com/eazylivings/activities/login/SignUp.java
@@ -1,9 +1,18 @@
 package com.eazylivings.activities.login;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.eazylivings.R;
+import com.eazylivings.VO.UserDetails;
+import com.eazylivings.constant.Constants;
+import com.eazylivings.databasehandler.ServerDatabaseHandler;
+import com.eazylivings.validator.Validator;
 
 public class SignUp extends AppCompatActivity {
 
@@ -11,5 +20,71 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void onClickRegisterNewUser(View view) {
+
+        final EditText userName = (EditText) findViewById(R.id.SignUpPage_editText_Name);
+        final EditText password = (EditText) findViewById(R.id.SignUpPage_editText_password);
+        final EditText emailAddress = (EditText) findViewById(R.id.SignUpPage_editText_email);
+        final EditText contactNumber = (EditText) findViewById(R.id.SignUpPage_editText_PhoneNo);
+
+        boolean isEmailFormatCorrect=Validator.checkEmailFormat(emailAddress);
+        boolean isUserAlreadyPresent=Validator.checkExistingUser(userName,getApplicationContext(),this);
+        boolean isUserNameFormatCorrect=Validator.checkUsernameFormat(userName);
+        boolean isPasswordFormatCorrect=Validator.checkPasswordFormat(password);
+        boolean isContactNumberCorrect= Validator.checkContactNumber(contactNumber);
+
+        if(true/*isUserNameFormatCorrect && isEmailFormatCorrect && !isUserAlreadyPresent && isPasswordFormatCorrect && isContactNumberCorrect*/){
+
+
+            UserDetails userDetails=new UserDetails();
+            userDetails.setUserName(userName.getText().toString());
+            userDetails.setPassword(password.getText().toString());
+            userDetails.setEmail_address(emailAddress.getText().toString());
+            userDetails.setContact_number(contactNumber.getText().toString());
+
+            ServerDatabaseHandler serverDatabaseHandler=new ServerDatabaseHandler(getApplicationContext(),this);
+            serverDatabaseHandler.execute(Constants.REGISTER,userName.getText().toString(),password.getText().toString(),emailAddress.getText().toString(),contactNumber.getText().toString());
+
+
+        }/*else if(isUserAlreadyPresent){
+            generatePopupMessages("This user is already present. Please sign in.");
+        }else if(!isUserNameFormatCorrect){
+            generatePopupMessages("Please check username");
+        }else if(!isEmailFormatCorrect){
+            generatePopupMessages("Please provide correct email address");
+        }else if(!isPasswordFormatCorrect){
+            generatePopupMessages("Please choose correct password");
+        }else if(!isContactNumberCorrect){
+            generatePopupMessages("Please provide correct contact number.");
+        }*/
+    }
+
+    private void generatePopupMessages(String message){
+        {
+            AlertDialog alertDialog = new AlertDialog.Builder(SignUp.this).create();
+            alertDialog.setTitle("Alert");
+            alertDialog.setMessage(message);
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
+    }
+    private void backButtonListener(Button backButton){
+
+        if (backButton != null) {
+            backButton.setOnClickListener(new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }
+
     }
 }
