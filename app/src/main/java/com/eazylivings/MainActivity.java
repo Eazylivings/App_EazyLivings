2a3ea1951e0aa849ddb9624aa9118c4fbb313916
Eazylivings <eazylivings01@gmail.com>
2016-05-29 18:04:38 +0530

    Navigation Drawer + MainActivity

--- app/src/main/java/com/eazylivings/MainActivity.java
+++ app/src/main/java/com/eazylivings/MainActivity.java
@@ -1,13 +1,38 @@
 package com.eazylivings;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.eazylivings.activities.WelcomeScreen;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        if(!prefs.getBoolean("loginStatus", false)){
+            prefs.edit().remove("userName");
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent;
+                mainIntent = new Intent(MainActivity.this,WelcomeScreen.class);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        }, 1000);
     }
 }
