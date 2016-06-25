bd6b91d9e55e62d82c852a482504a4c3fb97db6a
Eazylivings <eazylivings01@gmail.com>
2016-06-26 02:00:10 +0530

    Corousal

--- app/src/main/java/com/eazylivings/adapter/CustomSwipeAdapter.java
+++ app/src/main/java/com/eazylivings/adapter/CustomSwipeAdapter.java
@@ -3,13 +3,11 @@ package com.eazylivings.adapter;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
 
 import com.eazylivings.R;
 
@@ -23,38 +21,35 @@ public class CustomSwipeAdapter extends PagerAdapter {
 
     }
 
-
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        // TODO Auto-generated method stub
 
         LayoutInflater inflater = ((Activity)context).getLayoutInflater();
 
         View viewItem = inflater.inflate(R.layout.swipe_layout, container, false);
         ImageView imageView = (ImageView) viewItem.findViewById(R.id.welcomeScreen_textView_background);
         imageView.setImageResource(imageId[position]);
-        ((ViewPager)container).addView(viewItem);
+        (container).addView(viewItem);
 
         return viewItem;
     }
 
     @Override
     public int getCount() {
-        // TODO Auto-generated method stub
+
         return imageId.length;
     }
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        // TODO Auto-generated method stub
 
-        return view == ((View)object);
+        return view == object;
     }
 
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        // TODO Auto-generated method stub
-        ((ViewPager) container).removeView((View) object);
+
+        (container).removeView((View) object);
     }
 }
